ef546fb8b3c4840016d874a4096aad2bf39fe647
Stephen O'Dwyer <67125983+angrieralien@users.noreply.github.com>
2023-04-21 10:49:35 -0400

    fixes tests

--- processors/auditd/sessiontracker/sessiontracker.go
+++ processors/auditd/sessiontracker/sessiontracker.go
@@ -368,9 +368,6 @@ func (o *user) toAuditEvent(ae *aucoalesce.Event) *auditevent.AuditEvent {
 	if evt.Metadata.Extra == nil {
 		evt.Metadata.Extra = make(map[string]any)
 	}
-	if len(ae.Process.Args) > 0 {
-		evt.Metadata.Extra["process_args"] = ae.Process.Args
-	}
 
 	evt.LoggedAt = ae.Timestamp
 	evt.Metadata.AuditID = ae.Session
@@ -381,6 +378,10 @@ func (o *user) toAuditEvent(ae *aucoalesce.Event) *auditevent.AuditEvent {
 		"action": ae.Summary.Action,
 		"how":    ae.Summary.How,
 		"object": ae.Summary.Object,
+	}
+
+	if len(ae.Process.Args) > 0 {
+		evt.Metadata.Extra["process_args"] = ae.Process.Args
 	}
 
 	return evt
