880a4abdb3c65f67833e419496b3515857f3e671
hnadiminti-equinix <hari.nadiminti@eu.equinix.com>
2023-05-17 12:32:32 +0100

    update fix

--- processors/auditd/sessiontracker/errors.go
+++ processors/auditd/sessiontracker/errors.go
@@ -1,6 +1,6 @@
 package sessiontracker
 
-// SessionTrackerError is used to return errors pertaining to session audits
+// SessionTrackerError is used to return errors pertaining to session audits.
 type SessionTrackerError struct {
 	remoteLoginFail bool   // set when remote login cannot be validated
 	parsePIDFail    bool   // set when PID of the session cannot be parsed
@@ -9,12 +9,12 @@ type SessionTrackerError struct {
 	inner           error  // the error object
 }
 
-// RemoteLoginFailed returns true if the remote login validation has failed
+// RemoteLoginFailed returns true if the remote login validation has failed.
 func (o *SessionTrackerError) RemoteLoginFailed() bool {
 	return o.remoteLoginFail
 }
 
-// ParsePIDFailed returns true if the PID of the session could not be parsed
+// ParsePIDFailed returns true if the PID of the session could not be parsed.
 func (o *SessionTrackerError) ParsePIDFailed() bool {
 	return o.parsePIDFail
 }
