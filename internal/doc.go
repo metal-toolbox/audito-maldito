f1bf0ca85daf1af61986c70be6be36fbcec3f140
Hari N <99889761+hnadiminti-equinix@users.noreply.github.com>
2023-05-31 14:32:31 +0100

    documented requirements on the contents of the UserAction, UserAction event types and UserLogin audit event (#75)
    * documented requirements on the contents of the UserAction, UserAction event types and UserLogin audit event
    
    * rerunning the job
    
    * fixing lint issue
    
    * fix lint issue
    
    * added README.md at the package level
    
    * update fix
    
    * update fix
    
    * update fix
    
    * update fix
    
    * update fix
    
    * update fix
    
    * update fix
    
    * re-running the process
    
    * update codacy fix
    
    * update codacy fix
    
    * update codacy fix

---
+++ internal/doc.go
@@ -0,0 +1,2 @@
+// package internal contains the core logic for event audit
+package internal
--- processors/auditd/sessiontracker/errors.go
+++ processors/auditd/sessiontracker/errors.go
@@ -1,29 +1,35 @@
 package sessiontracker
 
+// SessionTrackerError is used to return errors pertaining to session audits.
 type SessionTrackerError struct {
-	remoteLoginFail bool
-	parsePIDFail    bool
-	auditWriteFail  bool
-	message         string
-	inner           error
+	remoteLoginFail bool   // set when remote login cannot be validated
+	parsePIDFail    bool   // set when PID of the session cannot be parsed
+	auditWriteFail  bool   // set when the audit event fails to write to event writer
+	message         string // the error message
+	inner           error  // the error object
 }
 
+// RemoteLoginFailed returns true if the remote login validation has failed.
 func (o *SessionTrackerError) RemoteLoginFailed() bool {
 	return o.remoteLoginFail
 }
 
+// ParsePIDFailed returns true if the PID of the session could not be parsed.
 func (o *SessionTrackerError) ParsePIDFailed() bool {
 	return o.parsePIDFail
 }
 
+// AuditEventWriteFailed returns true when the audit event write fails.
 func (o *SessionTrackerError) AuditEventWriteFailed() bool {
 	return o.auditWriteFail
 }
 
+// Error returns the error message.
 func (o *SessionTrackerError) Error() string {
 	return o.message
 }
 
+// Unwrap unwraps the error content.
 func (o *SessionTrackerError) Unwrap() error {
 	return o.inner
 }
